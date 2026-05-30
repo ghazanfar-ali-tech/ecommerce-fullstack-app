502ebd391665aec1239ec92fc70d113fdd79ebb0
ghazanfar-ali-tech <salam032136muet@gmail.com>
2026-05-30 11:37:52 +0500

    safe error resolved

--- python_backend/ecommerce_backend/ecommerce_backend/settings.py
+++ python_backend/ecommerce_backend/ecommerce_backend/settings.py
@@ -87,16 +87,9 @@ WSGI_APPLICATION = 'ecommerce_backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-      'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-        'OPTIONS': {
-            'sslmode': os.getenv("DB_SSLMODE", "require")
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 SAFEPAY_PUBLIC = os.getenv("SAFEPAY_PUBLIC")
