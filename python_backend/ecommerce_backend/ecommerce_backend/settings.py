096358f1dc45d62554ed6975aa8b365647ab4d5f
ghazanfar-ali-tech <salam032136muet@gmail.com>
2026-06-19 10:28:20 +0500

    new config files are added

--- python_backend/ecommerce_backend/ecommerce_backend/settings.py
+++ python_backend/ecommerce_backend/ecommerce_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-import dj_database_url
+
 
 from pathlib import Path
 
@@ -88,10 +88,10 @@ WSGI_APPLICATION = 'ecommerce_backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-    )
+    'default':{
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME':BASE_DIR / 'db.sqlite3'
+    }
 }
 SAFEPAY_PUBLIC = os.getenv("SAFEPAY_PUBLIC")
 SAFEPAY_SECRET = os.getenv("SAFEPAY_SECRET")
--- python_backend/ecommerce_backend/ecommerce_backend/wsgi.py
+++ python_backend/ecommerce_backend/ecommerce_backend/wsgi.py
@@ -1,7 +1,16 @@
+"""
+WSGI config for ecommerce_backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'ecommerce_backend.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'ecommerce_backend.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_backend.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 application = get_wsgi_application()
